7dc99d2199850c05628e03693ad592b78dfd7461
Vand <vand.filbert@gmail.com>
2020-06-11 14:59:28 +0800

    Updates

---
+++ precache-manifest.65860358006e24bfe5b8f56b123f8500.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cbe9d4a815d7eb8302b650c392e047",
+    "url": "/MyNewNewsApp/index.html"
+  },
+  {
+    "revision": "a76b994a00b1d47eb873",
+    "url": "/MyNewNewsApp/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "0dc1f8fef5a568a4245c",
+    "url": "/MyNewNewsApp/static/css/main.dfd813df.chunk.css"
+  },
+  {
+    "revision": "a76b994a00b1d47eb873",
+    "url": "/MyNewNewsApp/static/js/2.de94d620.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MyNewNewsApp/static/js/2.de94d620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0dc1f8fef5a568a4245c",
+    "url": "/MyNewNewsApp/static/js/main.85b5b050.chunk.js"
+  },
+  {
+    "revision": "b12e9da9d04cc1c29df3",
+    "url": "/MyNewNewsApp/static/js/runtime-main.efd1ca3a.js"
+  }
+]);
